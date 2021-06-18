b66972509fe46b4f65790e0be4b868537d0f8824
Matheus Willian <matheuswillian91@hotmail.com>
2021-06-17 21:00:14 -0300

    crud no bd

--- CursoUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ CursoUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -17,15 +17,6 @@ namespace CursoUdemy.Services.Implementations
             _context = context;
         }
 
-        public Person Create(Person person)
-        {
-            return person;
-        }
-
-        public void Delete(long id)
-        {
-        }
-
         public List<Person> FindAll()
         {
             return _context.People.ToList();
@@ -33,19 +24,66 @@ namespace CursoUdemy.Services.Implementations
 
         public Person FindById(long id)
         {
-            return new Person
+            return _context.People.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Create(Person person)
+        {
+            try
             {
-                Id = 1,
-                FirstName = "Matheus",
-                LastName = "Polato",
-                Address = "Bauru, SP",
-                Gender = "Male"
-            };
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.People.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
             return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.People.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.People.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.People.Any(p => p.Id.Equals(id));
         }
     }
 }
